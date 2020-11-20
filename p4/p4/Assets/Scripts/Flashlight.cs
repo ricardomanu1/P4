b8bf4484318473dbdd10021fa9d33fd14ed3d158
Ricardo <ricardo_31119@hotmail.com>
2020-11-20 14:33:48 +0100

    ja
    jajaja

---
+++ p4/p4/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public Light flashlight = null;
+
+    private void Start()
+    {
+        flashlight.enabled = false;
+    }
+    public void OnOff() 
+    {
+        flashlight.enabled = !flashlight.enabled;
+    }
+}
